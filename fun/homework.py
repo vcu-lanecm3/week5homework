05278152f5498d26033231600867841c43ef3404
vcu-Lanecm3 <lanecm3@vcu.edu>
2021-03-09 00:24:29 -0500

    Updated Code

--- fun/homework.py
+++ fun/homework.py
@@ -1,33 +1,46 @@
-"""Homework file for my students to have fun with some algorithms! """
+"""Homework file for my students to have fun with some algorithms!"""
 
 
 def find_greatest_number(incoming_list):
-    """Required parameter, incoming_list, should be a list. Find the largest number in the list.""" 
-    
-    Max_Number = max(incoming_list)
-    return Max_Number
+    """
+    Required parameter, incoming_list,
+    should be a list. Find the largest number in the list.
+    """
+
+    max_number = max(incoming_list)
+    return max_number
 
 
 def find_least_number(incoming_list):
-    """Required parameter, incoming_list, should be a list. Find the smallest/least number in the list."""
-    
-    Min_Number = min(incoming_list)
-    return Min_Number
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the smallest/least number in the list.
+    """
+
+    min_number = min(incoming_list)
+    return min_number
 
 
 def add_list_numbers(incoming_list):
-    """Required parameter, incoming_list, should be a list. Add all the values together and return it."""
-    
-    Sum = sum(incoming_list)
-    return Sum
+    """
+    Required parameter, incoming_list, should be a list.
+    Add all the values together and return it.
+    """
+
+    list_sum = sum(incoming_list)
+    return list_sum
 
 
 def longest_value_key(incoming_dict):
-    """Required parameter, incoming_dict, should be a dict. Find the KEY that has a value with the highest length, use the len() function"""
-    
-    keys = list(incoming_dict.keys())
-    Values = list(incoming_dict.values())
-    Longest_Value = max(Values, key = len)
-    Position = Values.index(Longest_Value)
-    Longest_Key = keys[Position]
-    return Longest_Key
+    """
+    Required parameter, incoming_dict, should be a dict.
+    Find the KEY that has a value with the highest length,
+    use the len() function
+    """
+
+    dictkeys = list(incoming_dict.keys())
+    dictvalues = list(incoming_dict.values())
+    longest_value = max(dictvalues, key = len)
+    position = dictvalues.index(longest_value)
+    longest_key = dictkeys[position]
+    return longest_key
